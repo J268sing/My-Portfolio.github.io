74c86a259b31e3cf99726f201ecae4a5d145f911
j268sing <j268sing@edu.uwaterloo.ca>
2020-02-02 22:27:34 -0500

    Updates

---
+++ precache-manifest.1bfdad3373ca86a6c82099e0a77700fb.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e33c957885e2a8a4cb69f97e3304aa7",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "5a87191d8259b2c7c6c5",
+    "url": "/My-Portfolio/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "8a0ff7c787d59c9f33a0",
+    "url": "/My-Portfolio/static/css/main.b2fc59fb.chunk.css"
+  },
+  {
+    "revision": "5a87191d8259b2c7c6c5",
+    "url": "/My-Portfolio/static/js/2.097024b3.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/My-Portfolio/static/js/2.097024b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8a0ff7c787d59c9f33a0",
+    "url": "/My-Portfolio/static/js/main.21911b9a.chunk.js"
+  },
+  {
+    "revision": "b33f96a3f5bb23d37b62",
+    "url": "/My-Portfolio/static/js/runtime-main.e67f7a83.js"
+  },
+  {
+    "revision": "48ce91f4cf428e04404fb2fa01cf8532",
+    "url": "/My-Portfolio/static/media/Flower-Prediction.48ce91f4.png"
+  },
+  {
+    "revision": "7b63a9782951c6d481d8e4290b222859",
+    "url": "/My-Portfolio/static/media/Flower-prediction1.7b63a978.png"
+  },
+  {
+    "revision": "4c73db57e0f32aea0008177d74f15dec",
+    "url": "/My-Portfolio/static/media/Flower-prediction2.4c73db57.png"
+  },
+  {
+    "revision": "f68808a11787f5b93e0bed2966926b88",
+    "url": "/My-Portfolio/static/media/TVscript.f68808a1.png"
+  },
+  {
+    "revision": "99eddb7c59373d53e0c5a69c541c79f0",
+    "url": "/My-Portfolio/static/media/ToDo.99eddb7c.png"
+  },
+  {
+    "revision": "18c0f0e68704cfdfea05334f3a8b36b2",
+    "url": "/My-Portfolio/static/media/ToDo1.18c0f0e6.png"
+  },
+  {
+    "revision": "8311e45afee604c3b088124cff3c0e68",
+    "url": "/My-Portfolio/static/media/ToDo2.8311e45a.png"
+  },
+  {
+    "revision": "05dc73abbdd00378eb337e06c8f66ae9",
+    "url": "/My-Portfolio/static/media/backend.05dc73ab.png"
+  },
+  {
+    "revision": "dacc02fa924eb8dd44f75078857c498d",
+    "url": "/My-Portfolio/static/media/final-transfer-learning.dacc02fa.png"
+  },
+  {
+    "revision": "44d0b47a7aa937b21f3eb1fcbfd86a82",
+    "url": "/My-Portfolio/static/media/frontend.44d0b47a.png"
+  },
+  {
+    "revision": "81275df04311b71be5f6761a8e6e7ef1",
+    "url": "/My-Portfolio/static/media/github.81275df0.png"
+  },
+  {
+    "revision": "9785e16d8d37050fd037341fcf5246a9",
+    "url": "/My-Portfolio/static/media/king.9785e16d.png"
+  },
+  {
+    "revision": "ef1bbc8f6ef9e0dfd28deeecdb1dda4a",
+    "url": "/My-Portfolio/static/media/me1.ef1bbc8f.png"
+  },
+  {
+    "revision": "29bfd78aba327f9a29696028443e36b7",
+    "url": "/My-Portfolio/static/media/neural-hd.29bfd78a.jpg"
+  },
+  {
+    "revision": "61f968fd37a1c41beeffa6e20334352b",
+    "url": "/My-Portfolio/static/media/neural-net.61f968fd.jpeg"
+  },
+  {
+    "revision": "c02e03d811cf207aedb80a73ce13b71e",
+    "url": "/My-Portfolio/static/media/transfer-learning-training.c02e03d8.png"
+  },
+  {
+    "revision": "8e4234f7fc689fb34fae2d74e5d8d6be",
+    "url": "/My-Portfolio/static/media/transfer-learning.8e4234f7.png"
+  },
+  {
+    "revision": "066b8fd692d3f132f396d56e616588c9",
+    "url": "/My-Portfolio/static/media/ui.066b8fd6.png"
+  }
+]);
